87fc6cf5c2dc98048242408cf59153a4740175f7
Diego Elio Pettenò <flameeyes@flameeyes.com>
2023-10-08 19:57:38 +0100

    Implement the basics of the application.
    It's basically just filling in the right MTM parameters for various
    social networks.

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,7 +1,31 @@
-// SPDX-FileCopyrightText: 2017 Jeremy Thomas
+// SPDX-FileCopyrightText: 2023 Diego Elio Pettenò
 //
 // SPDX-License-Identifier: MIT
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
-});
+export function inputChanged() {
+  let input = document.getElementById("inputUrl");
+
+  let inputURL = new URL(input.value);
+  inputURL.searchParams.set("mtm_campaign", "social");
+
+  let allInputURLs = document.getElementsByClassName("tagger-social-urls");
+
+  Array.prototype.forEach.call(allInputURLs, element => {
+    updateURL(inputURL, element);
+  });
+}
+
+function updateURL(inputURL, socialElement) {
+  inputURL.searchParams.set("mtm_kwd", socialElement.getAttribute("data-mtm-kwd"));
+
+  socialElement.value = inputURL.href;
+}
+
+export function copyURL(urlId) {
+  let urlInput = document.getElementById(urlId);
+
+  urlInput.select();
+  urlInput.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(urlInput.value);
+}
